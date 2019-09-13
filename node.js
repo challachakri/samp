9907b76d7f3cd8e0f289d1f1b2be821d986d7917
madhav411 <madhavguvvuala@gmail.com>
2019-09-13 13:04:20 +0000

    1st file is committing

---
+++ node.js
@@ -0,0 +1,11 @@
+Basic Commands of nodejs applications:
+=====================================
+npm install
+
+npx lerna bootstrap
+
+npx lerna bootstrap --scope <project-name>
+
+npx lerna run dev --scope <project-name> --stream
+
+npx lerna run build --scope <proeject-name> --stream
